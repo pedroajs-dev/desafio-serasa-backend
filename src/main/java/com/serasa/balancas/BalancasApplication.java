4e945af011b77876f7458568a27353e82ab61706
Pedro Augusto <pedroajs93@gmail.com>
2026-07-10 17:07:59 -0300

    docs(e1): add OpenAPI/Swagger documentation

--- src/main/java/com/serasa/balancas/BalancasApplication.java
+++ src/main/java/com/serasa/balancas/BalancasApplication.java
@@ -1,8 +1,14 @@
 package com.serasa.balancas;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@OpenAPIDefinition(info = @Info(
+		title = "Balancas API",
+		description = "Cadastre and transport-transaction management for Serasa's weighbridge (balanca) operations"
+))
 @SpringBootApplication
 public class BalancasApplication {
 
